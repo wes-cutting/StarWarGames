8532e2a2ece65037e20691275b4a143a94017752
Colten Rouska <crouska@gmail.com>
2013-05-06 18:58:45 -0600

    Committing Encrypton class
    Hopefully ignores other files

---
+++ WarGamesClient/src/main/java/edu/neumont/csc380/Encrypton.java
@@ -0,0 +1,18 @@
+package edu.neumont.csc380;
+
+public class Encrypton {
+	
+	//N Message
+	//E Public Key
+	//D Private Key
+	
+	public static String Encrypt(String n, String e) {//, String d) {
+		
+		return null;
+	}
+	
+	public static String Sign() {
+		
+		return null;
+	}
+}
