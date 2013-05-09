a9ede3c2f6c3de38ff3e6651191b00b89bc362cb
Joel Alexander <jkalexander67@gmail.com>
2013-05-08 23:37:36 -0600

    Client: Made changes to Client and Board classes
    Implementing :
    Client:
    Getting the game list
    Making a new game
    Making a move
    Checking if the game has ended
    Board:
    Now using char 2d array

--- WarGamesClient/src/main/java/edu/neumont/csc380/Board.java
+++ WarGamesClient/src/main/java/edu/neumont/csc380/Board.java
@@ -1,12 +1,15 @@
 package edu.neumont.csc380;
 
+import java.awt.Point;
 import java.util.Scanner;
 
-public class Board {
+public class Board extends modelEnities.Board{
 	public static final int BOARD_ROWS = 3;
 	public static final int BOARD_COLS = 3;
 
-	String[][] board = new String[3][3];
+	char[][] board = new char[3][3];
+	
+
 	Scanner scan = new Scanner(System.in);
 
 	/**
@@ -18,7 +21,7 @@ public class Board {
 	 * @return
 	 */
 	public boolean isValidMove(int row, int col) {
-		return board[col][row] == null;
+		return getBoard()[row][col] == (' ');
 	}
 
 	/**
@@ -27,25 +30,25 @@ public class Board {
 	 * @param piece
 	 * @return
 	 */
-	public String[][] placePiece(String piece) {
+	public Point placePiece() {
 		System.out.println("Where to move?");
 		boolean valid = false;
+		int row;
+		int col;
 		do {
 			System.out
 					.println("Enter the row and column of your move separated by a comma. Ex: 1,3");
 			drawBoard();
 			String[] move = scan.nextLine().split(",");
-			int row = Integer.parseInt(move[0]);
-			int col = Integer.parseInt(move[1]);
+			row = Integer.parseInt(move[0]);
+			col = Integer.parseInt(move[1]);
 			if (isValidMove(row, col)) {
-				board[row][col] = piece;
 				valid = true;
 			} else {
 				System.out.println("There's already a piece here, try again.");
 			}
 		} while (!valid);
-		drawBoard();
-		return board;
+		return new Point(row, col);
 	}
 
 	/**
@@ -59,4 +62,15 @@ public class Board {
 			System.out.println();
 		}
 	}
+	@Override
+	public char[][] getBoard(){
+		return board;
+	}
+	
+	@Override
+	public void setBoard(char[][] board) {
+		this.board = board;
+	}
+	
+	
 }
--- WarGamesClient/src/main/java/edu/neumont/csc380/Client.java
+++ WarGamesClient/src/main/java/edu/neumont/csc380/Client.java
@@ -1,62 +1,123 @@
 package edu.neumont.csc380;
 
+import java.awt.Point;
+import java.math.BigInteger;
+import java.util.List;
 import java.util.Scanner;
 
+import modelRequests.*;
+import modelResponses.*;
+import org.apache.camel.ProducerTemplate;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 @ContextConfiguration(locations = "beans.xml")
-public class Client extends AbstractJUnit4SpringContextTests{
-	
+public class Client extends AbstractJUnit4SpringContextTests {
+	@Autowired
+	ProducerTemplate camelTemplate;
+	Board board = null;
+	List<Integer> gameList = null;
+
 	@Test
 	public void test() {
 		start();
+		do{
+			
+		}while();
 	}
-	
+
 	/**
 	 * @param args
 	 */
-	
+
 	Scanner input = new Scanner(System.in);
-	//add a menu state?
-	
-	public void start(){
-			String startMode = new PromptHandler().start() + "";
-			switch(startMode){
-			case "11":
-				//Send request to start a game with easy AI
-				break;
-			case "12":
-				//Send request to start a game with medium AI
-				break;
-			case "13":
-				//Send request to start a game with hard AI
-				break;
-			case "21":
-				//Send request to retrieve to create a game
-				break;
-			case "22":
-				//Send request to retrieve all active games
-				break;
-			}
-		getActiveGames();
+
+	// add a menu state?
+
+	public void start() {
+		// GetKeys
+		BigInteger publicKey = null;
+		String startMode = new PromptHandler().start() + "";
+		switch (startMode) {
+		
+		case "11":
+			// Send request to start a game with easy AI
+
+			break;
+		case "12":
+			// Send request to start a game with medium AI
+
+			break;
+		case "13":
+			// Send request to start a game with hard AI
+			
+			break;
+		case "21":
+			// Send request to retrieve to create a game
+			board = makeNewGame(publicKey);
+			break;
+		case "22":
+			// Send request to retrieve all active games
+			gameList = getActiveGames(publicKey);
+			break;
+		}
 	}
-	
+
 	/**
 	 * Gets all active games
 	 */
-	public void getActiveGames(){
-		
+	public List<Integer> getActiveGames(BigInteger publicKey) {
+		GameListRequest request = new GameListRequest();
+		request.setPublicKey(publicKey);			
+		GameListResponse response = camelTemplate.requestBody("jms:queue:game",
+				request, GameListResponse.class);
+		return response.getGameList();
+	}
+
+	/**
+	 * Makes a new game request and gets the board
+	 */
+	public Board makeNewGame(BigInteger publicKey) {
+		NewGameRequest request = new NewGameRequest();
+		request.setPublicKey(publicKey);
+		NewGameResponse response = camelTemplate.requestBody("jms:queue:game",
+				request, NewGameResponse.class);
+		return (Board) response.getBorad();
 	}
 	
 	/**
-	 * Retrieves the public and private keys from Key Generation Service
+	 * Sends a move request
+	 * @param publicKey used to sign the request
+	 * @return
 	 */
-	public void getKeys(){
+	public Board makeMove(BigInteger publicKey){
 		
+		Point pos = board.placePiece();
+		MoveRequest request = new MoveRequest();
+		request.setMoveRow(pos.x);
+		request.setMoveCol(pos.y);
+		request.setPublicKey(publicKey);
+		MoveResponse response = camelTemplate.requestBody("jms:queue:game",
+				request, MoveResponse.class);
+		return (Board) response.getBoard();
 	}
 	
-	
-	
+	/**
+	 * Request to check for the end game
+	 * @return
+	 */
+	public BigInteger hasGameEnded(){
+		//
+		EndGameResponse response = camelTemplate.requestBody("jms:queue:game", EndGameResponse.class);
+	}
+
+	/**
+	 * Retrieves the public and private keys from Key Generation Service
+	 */
+	public BigInteger getKeys() {
+		return 
+	}
+
 }
